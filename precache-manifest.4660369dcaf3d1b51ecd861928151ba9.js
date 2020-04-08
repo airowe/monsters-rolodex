a7d487a91171d652f84e1e0d8766eef2b4bd6d93
Adam Rowe <adaminsley@gmail.com>
2020-04-08 10:41:23 -0400

    Updates

---
+++ precache-manifest.4660369dcaf3d1b51ecd861928151ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a4bdbb7e1c8e1352670accf2d1e688",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c29801ccf5b0b470b7e",
+    "url": "/monsters-rolodex/static/css/main.224d95e0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c29801ccf5b0b470b7e",
+    "url": "/monsters-rolodex/static/js/main.0b33e7ab.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
